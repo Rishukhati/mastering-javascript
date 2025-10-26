5defdf8459be9f18bc07aed61c9150d3a04e8cdf
Rishabh singh <rishabhsingh.block@gmail.com>
2025-10-26 15:42:48 +0530

    Till #45 for in loop

--- mastering-javascript/ch-3/appp.js
+++ mastering-javascript/ch-3/appp.js
@@ -61,3 +61,96 @@ const user = {
 };
 const cloneUser = {...user};
 console.log(cloneUser);
+
+function value(...param) {
+    console.log(param);
+
+}
+value (1 , 2 ,3 ,4,5);
+
+//  Destructuring
+// inArray
+const colors = ['red' , 'green' ,'blue' , 'yellow'];
+
+const [color1,color2,color3] = colors;
+console.log(color1 , color2,color3);  
+
+// inFunction array
+function f() {
+    return[1,2];
+}
+let a,b;
+[a, b] = f();
+console.log(a,b);
+
+// inObject
+const person ={
+    name : 'rishabh',
+    age: 30,
+    gender : 'male',
+    country :'New york',
+
+};
+const {name: naam, age , country} = person;
+console.log(naam);
+console.log(`age : ${age}`); //when we want to write like this we have to use backtick
+console.log("country :" , country);
+
+// infuntion
+
+let option = {
+    title : 'My menu',
+    items : ["item1", "item2"]
+};
+
+function showMenu({title = untitled , items}) {
+    console.log(`${title}`);
+    console.log(`${items}`);
+}
+
+showMenu(option);
+
+// nestedDestructuring
+
+ const song = [
+    {name : 'r'},
+    {name : 'i'},
+    {name: 's'},
+    {name: 'h'}
+]
+const [, ,{name}] = song;
+console.log(name);
+
+// #44: Ternary Operator 
+// if and else alternative not use for if and else if
+let pass = 8;
+
+function passwordChecker(ps) {
+    // if (ps === 8) {
+    // return 'strong password';    
+    // } else {
+    //     return 'pass should be 8 character';
+    // }
+    return ps === 8 ? 'strong password' : 'pass should be 8 character';
+}
+const res = passwordChecker(pass);
+console.log(res);
+
+const agee = 25;
+// ? - then  : - else
+const isAdult = agee>=18 ? 'Adult' : 'Not an Adult';
+console.log(isAdult);
+
+// ex -
+
+let personMoney = true;
+let haveMoney =
+    personMoney === true ? 'buy products' : 'they bring money';
+    console.log(haveMoney);
+
+// #45 for in loop
+const object = {a:1 , b:2 ,c:3};
+for (let number in object) {
+        // console.log(`${number}: ${object[number]}`);
+        console.log(number , object[number]);
+}
