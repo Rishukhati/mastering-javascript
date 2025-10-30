7bfd22f29f66316a74dc718ae4486b75f1af8e54
Rishabh singh <rishabhsingh.block@gmail.com>
2025-10-30 20:09:55 +0530

    understand till classlist folder

--- mastering-javascript/Advance/DOM/03. DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
+++ mastering-javascript/Advance/DOM/03. DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,12 @@
 // innerText
 // textContent
 // innerHTML
+
+const p = document.querySelector("p");
+console.log(p.innerText);
+console.log(p.textContent);
+console.log(p.innerHTML);
+
+const h1 = document.querySelector("h1");
+h1.innerHTML = "<em> Rishabh WebDev </em>";
+// h1.innerText = "text changed";
