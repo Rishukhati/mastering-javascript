f8f16f5d3f1ba7f7c8df284feb51c1df0f7a7986
Rishabh singh <rishabhsingh.block@gmail.com>
2025-10-30 08:00:02 +0530

    DOM

---
+++ mastering-javascript/Advance/DOM/dom.js
@@ -0,0 +1,31 @@
+// Document Object Model
+// ************ DOM SELECTORS ************
+// 1. getElementsByTagName
+
+// console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementsByTagName("h1").length);
+
+// 2. getElementById
+
+// console.log(document.getElementById("main"));
+
+// 3. getElementsByClassName
+
+// console.log(document.getElementsByClassName("cls"));
+
+// 4. querySelector
+// tagname -- normal write ""
+// id -- .nameofid
+// class -- #namrofclass
+
+console.log(document.querySelector(".cls"));
+
+// 5. querySelectorAll
+
+console.log(document.querySelectorAll("li"));
+console.log(document.querySelectorAll("li").length);
+
+// storing data in variables
+
+const apple = document.querySelector("h1");
+console.log(apple);
