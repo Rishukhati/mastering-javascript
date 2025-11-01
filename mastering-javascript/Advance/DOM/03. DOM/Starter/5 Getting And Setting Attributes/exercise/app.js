e1420311daf05a6f6e6c52ddce4d458f257472b6
Rishabh singh <rishabhsingh.block@gmail.com>
2025-11-01 23:39:37 +0530

    update on DOM

--- mastering-javascript/Advance/DOM/03. DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
+++ mastering-javascript/Advance/DOM/03. DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
@@ -2,3 +2,8 @@
 // 2. Use getAttribute(attrName) to check the attribute.
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+const a = document.querySelector("a");
+const input = document.querySelector(input);
+
+console.log(a.getAttribute("href"));
